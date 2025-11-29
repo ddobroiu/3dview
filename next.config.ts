908c48286392fb16721aaa7a804279b15791ece5
ddobroiu <d_dobroiu@YAHOO.COM>
2025-11-29 05:15:39 +0200

    a

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,21 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // IGNORE lint errors during build (temporary)
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.3dview.ai',
+          },
+        ],
+        destination: 'https://3dview.ai/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
